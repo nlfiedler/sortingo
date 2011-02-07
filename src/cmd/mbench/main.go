2611604d07c40c5e96a21889b2a68c6778cc20b5
Nathan Fiedler <nlfiedler@yahoo.com>
2011-02-06 18:38:16 -0800

    Adding command line flags for mbench command.

--- src/cmd/mbench/main.go
+++ src/cmd/mbench/main.go
@@ -8,8 +8,11 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
+	"os"
 	"rand"
+	"regexp"
 	"sort"
 	"sortingo"
 	"strings"
@@ -139,9 +142,83 @@ func init() {
 	dataSets["Genome"] = genomeStrings
 }
 
+func usage() {
+        fmt.Println("Usage: mbench [options]")
+        fmt.Println("\t--data <regex>")
+        fmt.Println("\t\tSelect the data set whose name matches the regular expression.")
+        fmt.Println("\t\tFor example, '--data random' would use only the random data set.")
+        fmt.Println("\t--help")
+        fmt.Println("\t\tDisplay this usage information.")
+        fmt.Println("\t--list")
+        fmt.Println("\t\tDisplay a list of the supported data sets and sorting algorithms.")
+        fmt.Println("\t--sort <regex>")
+        fmt.Println("\t\tSelect the sort algorithms whose name matches the regular")
+        fmt.Println("\t\texpression. For example, '--sort (comb|insert)' would run")
+        fmt.Println("\t\tboth versions of the insertion and comb sort algorithms.")
+}
+
 // main runs the micro benchmarks on the "slower" sorting algorithms
 // using small data sets (under 500 elements).
 func main() {
+	var help = flag.Bool("help", false, "show usage information")
+	var list = flag.Bool("list", false, "list supported data sets and algorithms")
+	var data = flag.String("data", "", "regex to select data sets to sort")
+	var algo = flag.String("sort", "", "regex to select sorting algorithms")
+	flag.Parse()
+
+	if *help {
+		usage()
+		os.Exit(0)
+	}
+
+	if *list {
+	        fmt.Println("Data sets")
+		for _, dataSetName := range dataSetNames {
+                        fmt.Printf("\t%s\n", dataSetName)
+                }
+                fmt.Println("Sorting algorithms")
+		for _, sorterName := range sorterNames {
+                        fmt.Printf("\t%s\n", sorterName)
+                }
+                os.Exit(0)
+	}
+
+	if *data != "" {
+		re, err := regexp.Compile(strings.ToLower(*data))
+		if err != nil {
+			fmt.Printf("%s in '%s' of --data flag\n", err, *data)
+			os.Exit(1)
+		}
+		newData := make([]string, 0, len(dataSetNames))
+		for _, dataSetName := range dataSetNames {
+			if idx := re.FindStringIndex(strings.ToLower(dataSetName)); idx != nil {
+				newData = append(newData, dataSetName)
+			}
+		}
+		dataSetNames = newData
+	}
+
+	if *algo != "" {
+		re, err := regexp.Compile(strings.ToLower(*algo))
+		if err != nil {
+			fmt.Printf("%s in '%s' of --sort flag\n", err, *algo)
+			os.Exit(1)
+		}
+		newSort := make([]string, 0, len(sorterNames))
+		for _, sorterName := range sorterNames {
+			if idx := re.FindStringIndex(strings.ToLower(sorterName)); idx != nil {
+				newSort = append(newSort, sorterName)
+			}
+		}
+		sorterNames = newSort
+	}
+
+        // Avoid recreating the input arrays over and over again.
+	inputSets := make(map[int][]string)
+	for _, size := range sortSizes {
+		inputSets[size] = make([]string, size)
+	}
+
 	// For each type of data set...
 	// and each data set size...
 	// and each sort implementation...
@@ -150,7 +227,7 @@ func main() {
 		fmt.Printf("%s...\n", dataSetName)
 		dataSet := dataSets[dataSetName]
 		for _, size := range sortSizes {
-			input := make([]string, size)
+			input := inputSets[size]
 			fmt.Printf("\t%d...\n", size)
 			for _, sorterName := range sorterNames {
 				sorter := sorters[sorterName]
