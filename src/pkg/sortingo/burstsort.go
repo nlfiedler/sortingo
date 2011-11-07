d2453f6beddbedad246176fd3f82b23522ff7189
Nathan Fiedler <nlfiedler@yahoo.com>
2011-11-06 16:51:00 -0800

    Formatting recently added code.

--- src/pkg/sortingo/burstsort.go
+++ src/pkg/sortingo/burstsort.go
@@ -50,13 +50,13 @@ type burstNode struct {
 	// the last null bucket in the chain, starting from the
 	// first pointer in elements[0]
 	nulltail bucket
-        // counts is the number of items in each bucket,
+	// counts is the number of items in each bucket,
 	// or -1 if reference to trie node; for the null
 	// bucket it is the total size of all null buckets
 	// in the chain.
-        counts [alphabetSize]int
-        // pointers to buckets or trie node
-        elements [alphabetSize]interface{}
+	counts [alphabetSize]int
+	// pointers to buckets or trie node
+	elements [alphabetSize]interface{}
 }
 
 // append adds the given string into the appropriate bucket, using the
@@ -65,28 +65,28 @@ type burstNode struct {
 // character, in which case the string is added to the null bucket.
 // Buckets are expanded as needed to accomodate the new string.
 func (n *burstNode) append(c uint8, s string) {
-        // are buckets already created?
-        if n.counts[c] < 1 {
-                // need to create bucket
-                if c == nullterm {
+	// are buckets already created?
+	if n.counts[c] < 1 {
+		// need to create bucket
+		if c == nullterm {
 			// allocate memory for the null bucket,
 			// which is always sized at the maximum
 			n.nulltail = make(bucket, 1, threshold)
 			n.nulltail[0] = s
 			n.elements[c] = n.nulltail
 			n.counts[c]++
-                } else {
+		} else {
 			// allocate memory for the bucket
 			cs := make(bucket, 1, bucketStartSize)
 			cs[0] = s
 			n.elements[c] = cs
 			n.counts[c]++
-                }
-        } else {
-                // bucket already created
-                if (c == nullterm) {
+		}
+	} else {
+		// bucket already created
+		if c == nullterm {
 			// check if the bucket has reached the threshold
-			if (n.counts[c] % thresholdMinusOne == 0) {
+			if n.counts[c]%thresholdMinusOne == 0 {
 				// grow the null bucket by daisy chaining a new slice
 				tmp := make(bucket, 0, threshold)
 				n.nulltail = append(n.nulltail, tmp)
@@ -96,37 +96,37 @@ func (n *burstNode) append(c uint8, s string) {
 			// insert string in bucket and increment the item counter
 			n.nulltail = append(n.nulltail, s)
 			n.counts[c]++
-                } else {
+		} else {
 			cs := n.elements[c].(bucket)
 			// when bucket fills, increase its size up to the threshold
 			l := len(cs)
-			if (n.counts[c] < threshold && n.counts[c] == l) {
-				cs = cs.realloc(l, l * bucketGrowthFactor)
+			if n.counts[c] < threshold && n.counts[c] == l {
+				cs = cs.realloc(l, l*bucketGrowthFactor)
 			}
 			// insert string in bucket and increment the item counter
 			n.elements[c] = append(cs, s)
 			n.counts[c]++
-                }
-        }
+		}
+	}
 }
 
 // get retrieves either a trie node or a bucket for the given character.
 func (n *burstNode) get(c uint8) interface{} {
-        return n.elements[c]
+	return n.elements[c]
 }
 
 // set sets the trie node or bucket for a character.
 func (n *burstNode) set(c uint8, o interface{}) {
-        n.elements[c] = o
+	n.elements[c] = o
 	if _, is_node := o.(burstNode); is_node {
-                // flag to indicate pointer to trie node and not bucket
-                n.counts[c] = -1
-        }
+		// flag to indicate pointer to trie node and not bucket
+		n.counts[c] = -1
+	}
 }
 
 // size gets the number of strings stored for the given character.
 func (n *burstNode) size(c uint8) int {
-        return n.counts[c]
+	return n.counts[c]
 }
 
 //
