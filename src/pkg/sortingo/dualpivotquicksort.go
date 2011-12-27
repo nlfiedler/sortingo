6881e5a319306e563ae2d188f3581669571ffee5
Nathan Fiedler <nlfiedler@yahoo.com>
2011-12-26 18:05:48 -0800

    Simplifying the code for dual-pivot quicksort.

--- src/pkg/sortingo/dualpivotquicksort.go
+++ src/pkg/sortingo/dualpivotquicksort.go
@@ -9,16 +9,21 @@ package sortingo
 // DualPivotQuickSort will sort the given slice of strings using the
 // two pivot value quicksort variation by Vladimir Yaroslavskiy.
 func DualPivotQuickSort(a []string) {
-	dualPivotQuicksort(a, 0, len(a)-1)
+	size := len(a)
+	if a == nil || size < 2 {
+		return
+	}
+	dualPivotQuicksort(a, 0, size-1)
 }
 
 // dualPivotQuicksort is a variation of quicksort that uses two pivot
 // values rather than one, and was created by Vladimir Yaroslavskiy.
-// This Go implementation is a translation of the original Java code.
+// This Go implementation is a translation of the original Java code,
+// with some simplification of the code.
 func dualPivotQuicksort(a []string, left int, right int) {
 	len := right - left
 
-	// insertion sort on tiny array
+	// perform insertion sort on small ranges
 	if len < 17 {
 		for i := left + 1; i <= right; i++ {
 			for j := i; j > left && a[j] < a[j-1]; j-- {
@@ -28,7 +33,7 @@ func dualPivotQuicksort(a []string, left int, right int) {
 		return
 	}
 
-	// median indexes
+	// compute indices of medians
 	sixth := len / 6
 	m1 := left + sixth
 	m2 := m1 + sixth
@@ -36,7 +41,7 @@ func dualPivotQuicksort(a []string, left int, right int) {
 	m4 := m3 + sixth
 	m5 := m4 + sixth
 
-	// 5-element sorting network
+	// order the medians in preparation for partitioning
 	if a[m1] > a[m2] {
 		a[m1], a[m2] = a[m2], a[m1]
 	}
@@ -65,29 +70,23 @@ func dualPivotQuicksort(a []string, left int, right int) {
 		a[m4], a[m5] = a[m5], a[m4]
 	}
 
-	// pivots: [ < pivot1 | pivot1 <= && <= pivot2 | > pivot2 ]
+	// select the pivots such that [ < pivot1 | pivot1 <= && <= pivot2 | > pivot2 ]
 	pivot1 := a[m2]
 	pivot2 := a[m4]
 
 	diffPivots := pivot1 != pivot2
 
+	// move the pivots out of the away
 	a[m2] = a[left]
 	a[m4] = a[right]
-
-	// center part pointers
 	less := left + 1
 	great := right - 1
 
-	// sorting
+	// partition the elements
 	if diffPivots {
 		for k := less; k <= great; k++ {
 			x := a[k]
-
-			if x < pivot1 {
-				a[k] = a[less]
-				a[less] = x
-				less++
-			} else if x > pivot2 {
+			if x > pivot2 {
 				for a[great] > pivot2 && k < great {
 					great--
 				}
@@ -95,26 +94,20 @@ func dualPivotQuicksort(a []string, left int, right int) {
 				a[great] = x
 				great--
 				x = a[k]
-
-				if x < pivot1 {
-					a[k] = a[less]
-					a[less] = x
-					less++
-				}
+			}
+			if x < pivot1 {
+				a[k] = a[less]
+				a[less] = x
+				less++
 			}
 		}
 	} else {
 		for k := less; k <= great; k++ {
 			x := a[k]
-
 			if x == pivot1 {
 				continue
 			}
-			if x < pivot1 {
-				a[k] = a[less]
-				a[less] = x
-				less++
-			} else {
+			if x > pivot1 {
 				for a[great] > pivot2 && k < great {
 					great--
 				}
@@ -122,51 +115,44 @@ func dualPivotQuicksort(a []string, left int, right int) {
 				a[great] = x
 				great--
 				x = a[k]
-
-				if x < pivot1 {
-					a[k] = a[less]
-					a[less] = x
-					less++
-				}
+			}
+			if x < pivot1 {
+				a[k] = a[less]
+				a[less] = x
+				less++
 			}
 		}
 	}
 
-	// swap
+	// swap the pivots back into position
 	a[left] = a[less-1]
 	a[less-1] = pivot1
 	a[right] = a[great+1]
 	a[great+1] = pivot2
 
-	// left and right parts
+	// recursively sort the left and right partitions
 	dualPivotQuicksort(a, left, less-2)
 	dualPivotQuicksort(a, great+2, right)
 
-	// equal elements
+	// order the equal elements in the middle
 	if great-less > len-13 && diffPivots {
 		for k := less; k <= great; k++ {
 			x := a[k]
-
-			if x == pivot1 {
-				a[k] = a[less]
-				a[less] = x
-				less++
-			} else if x == pivot2 {
+			if x == pivot2 {
 				a[k] = a[great]
 				a[great] = x
 				great--
 				x = a[k]
-
-				if x == pivot1 {
-					a[k] = a[less]
-					a[less] = x
-					less++
-				}
+			}
+			if x == pivot1 {
+				a[k] = a[less]
+				a[less] = x
+				less++
 			}
 		}
 	}
 
-	// center part
+	// recursively sort the middle partition
 	if diffPivots {
 		dualPivotQuicksort(a, less, great)
 	}
